3a1bd716161262bc4ead4041a78eb003c609ea8f
BossOfTheCode <c.a.tulenov@gmail.com>
2022-01-08 00:48:38 +0500

    added 9. Palindrome Number

---
+++ easy/Palindrome_Number_9/Solution.java
@@ -0,0 +1,8 @@
+package Palindrome_Number_9;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        StringBuilder sb = new StringBuilder(String.valueOf(x));
+        return sb.toString().equals(sb.reverse().toString());
+    }
+}
